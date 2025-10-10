63e02bd098bfa351b2914dff7adc962219fd2555
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-10-10 10:15:36 +0200

    added prettier

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -1,49 +1,39 @@
+import Image from "next/image";
+import { Box } from "@mui/material";
+import { JSX } from "react";
 
-import Image from 'next/image';
-import { Box } from '@mui/material';
-import { JSX } from 'react';
-
-
-const MIN_SIZE = 100; 
-const MAX_SIZE = 200; 
+const MIN_SIZE = 100;
+const MAX_SIZE = 200;
 
 const boxStyles = {
-  width: {
-    xs: MIN_SIZE, 
-    sm: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`, 
-    md: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
-    lg: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
-    xl: MAX_SIZE,
-  },
-  height: {
-    xs: MIN_SIZE,
-    sm: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
-    md: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
-    lg: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
-    xl: MAX_SIZE,
-  },
-  borderRadius: '50%', 
-  overflow: 'hidden', 
-  marginBottom: '16px',
+    width: {
+        xs: MIN_SIZE,
+        sm: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
+        md: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
+        lg: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
+        xl: MAX_SIZE,
+    },
+    height: {
+        xs: MIN_SIZE,
+        sm: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
+        md: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
+        lg: `clamp(${MIN_SIZE}px, 16vw, ${MAX_SIZE}px)`,
+        xl: MAX_SIZE,
+    },
+    borderRadius: "50%",
+    overflow: "hidden",
+    marginBottom: "16px",
 };
 
 export default function About(): JSX.Element {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-4xl font-bold mb-4">O autorze</h1>
+            <Box sx={boxStyles}>
+                <Image src="/images/author.webp" alt="Author" width={MAX_SIZE} height={MAX_SIZE} style={{ objectFit: "cover" }} sizes={`(max-width: 600px) ${MIN_SIZE}px, (min-width: 1536px) ${MAX_SIZE}px, 16vw`} quality={75} />
+            </Box>
 
-  return (
-    <div className="container mx-auto p-4"> 
-      <h1 className="text-4xl font-bold mb-4">O autorze</h1>
-        <Box sx={boxStyles}>
-          <Image src="/images/author.webp" alt="Author" 
-        width={MAX_SIZE}
-        height={MAX_SIZE}
-        style={{ objectFit: 'cover' }} 
-        sizes={`(max-width: 600px) ${MIN_SIZE}px, (min-width: 1536px) ${MAX_SIZE}px, 16vw`}
-        quality={75}/>
-        </Box>
-     
-      <p className="text-lg">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
-    </div>
-  );
+            <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    );
 }
