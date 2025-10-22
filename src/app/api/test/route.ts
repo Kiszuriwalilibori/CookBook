8222dc934cd9f5962bc6b16c06bc9be202bf3ce5
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-10-22 14:54:32 +0200

    api/test/route.ts commented

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,11 +1,12 @@
-import { client } from "@/lib/sanity";
-import { NextResponse } from "next/server";
+// import { client } from "@/lib/sanity";
+// import { NextResponse } from "next/server";
 
-export async function GET() {
-    try {
-        const data = await client.fetch('*[_type == "test"]');
-        return NextResponse.json({ data });
-    } catch (error) {
-        return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 });
-    }
-}
+// export async function GET() {
+//     try {
+//         const data = await client.fetch('*[_type == "test"]');
+//         return NextResponse.json({ data });
+//     } catch (error) {
+//         return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 });
+//     }
+// }
+export default {};
