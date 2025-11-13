a8d134cd70f1ecb3a37224b88a968623503f26fe
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-11-13 18:59:26 +0100

    removing old code

--- src/stores/useAdminStore.ts
+++ src/stores/useAdminStore.ts
@@ -1,18 +1,3 @@
-// import { create } from "zustand";
-
-// interface AdminStore {
-//     isAdminLogged: boolean;
-//     setAdminLogged: (value: boolean, reason: string) => void;
-// }
-
-// export const useAdminStore = create<AdminStore>(set => ({
-//     isAdminLogged: false,
-//     setAdminLogged: (value: boolean, reason: string) => {
-//         set({ isAdminLogged: value });
-//         console.log(`[Admin Store Update] ${reason}. New state: isAdminLogged = ${value}`);
-//     },
-// }));
-
 import { create } from "zustand";
 
 interface AdminStore {
