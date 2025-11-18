385a8f29f56f8959afa9cbf4eaa53693380667c8
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-11-18 09:26:19 +0100

    fixed userchecker

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ import { Box } from "@mui/material";
 import { overlayStyles, modalStyles } from "./Header.styles";
 import { useRecipesSummary } from "@/hooks";
 import { Options } from "@/types";
-// import UserChecker from "../UserChecker";
+
 
 interface HeaderProps {
     initialSummary?: Options | null;
@@ -56,8 +56,7 @@ const Header = ({ initialSummary, fetchError }: HeaderProps) => {
     return (
         <>
             <Menu navItems={navItems} />
-            {/* <UserChecker /> */}
-
+            
             {showFilter && (
                 <>
                     <Box sx={overlayStyles} onClick={handleClose}>
