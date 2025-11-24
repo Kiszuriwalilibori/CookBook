7fe6436c1df880fb8abe8ae13ecc0184dd1815c8
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-11-24 19:11:43 +0100

    more codecommented for Vercel

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,6 @@
 import { getRecipeBySlug } from "./getRecipeBySlug";
 import { getRecipesForCards } from "./getRecipesForCards";
-import { getRecipesSummary } from "./getRecipesSummary";
 import { getSummary } from "./getSummary";
 import { fetchSummary } from "./fetchSummary";
-import { fetchRecipesSummarySafe } from "./fetchRecipesSummarySafe";
 
-export { getRecipesForCards, getRecipeBySlug, getRecipesSummary, getSummary, fetchSummary, fetchRecipesSummarySafe };
+export { getRecipesForCards, getRecipeBySlug, getSummary, fetchSummary };
