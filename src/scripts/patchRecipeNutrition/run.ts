e70fd7aecf38b95e12ca1263ae6d05f7e26bc3d8
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2026-02-08 16:27:47 +0100

    test

--- src/scripts/patchRecipeNutrition/run.ts
+++
@@ -1,8 +0,0 @@
-import { exampleRecipe } from "./exampleRecipe";
-import { patchNutritionByTitle } from "./patchNutritionByTitle";
-
-async function main() {
-    await patchNutritionByTitle(exampleRecipe);
-}
-
-main().catch(console.error);
