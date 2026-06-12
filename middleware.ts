10cc10f202c9fdbe1eed996e6f9a43e4df8a33e8
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2026-06-12 10:36:22 +0200

    test

--- middleware.ts
+++ middleware.ts
@@ -16,20 +16,18 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    console.log("MIDDLEWARE HIT:", request.nextUrl.pathname);
-
     const headers = new Headers(request.headers);
     headers.set("x-current-path", request.nextUrl.pathname);
 
     const response = NextResponse.next({ headers });
-    console.log("response", response);
+
     const existingUserId = request.cookies.get("userId")?.value;
 
     if (!existingUserId) {
         response.cookies.set("userId", crypto.randomUUID(), {
             httpOnly: true,
             sameSite: "lax",
-            secure: process.env.NODE_ENV === "production",
+            secure: true,
             path: "/",
             maxAge: 60 * 60 * 24 * 365,
         });
@@ -37,3 +35,7 @@ export function middleware(request: NextRequest) {
 
     return response;
 }
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
